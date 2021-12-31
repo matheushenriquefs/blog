934a8b46729ee68448c85802c171f663d5657739
matheushenriquefs <matheus.henrique1021@gmail.com>
2021-12-31 20:42:41 -0300

    Add PostList unit test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import { PostList } from "../src/components/PostList";
 import PostService from "../src/services/PostService";
 import PostRepository from "../src/repositories/PostRepository";
---
+++ src/components/PostList/PostList.test.js
@@ -0,0 +1,37 @@
+import "../../../__mocks__/matchMediaMock";
+import { render, screen } from "../../utils/test";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
+import { PostList } from "./PostList";
+
+test("loads and display component", async () => {
+  const mockedPosts = [
+    {
+        title: 'Title for my first post',
+        excerpt: 'Excerpt for my first post',
+        slug: 'first-post',
+        permalink: '/posts/first-post',
+        createdAt: '12-31-2021',
+    },
+    {
+        title: 'Title for my second post',
+        excerpt: 'Excerpt for my second post',
+        slug: 'second-post',
+        permalink: '/posts/second-post',
+        createdAt: '12-31-2021',
+    },
+    {
+        title: 'Title for my third post',
+        excerpt: 'Excerpt for my third post',
+        slug: 'third-post',
+        permalink: '/posts/third-post',
+        createdAt: '12-31-2021',
+    },
+  ];
+
+  const { container } = render(<PostList posts={mockedPosts} />);
+  const root = container.firstChild;
+
+  expect(root).toBeInTheDocument();
+
+  screen.debug();
+});
