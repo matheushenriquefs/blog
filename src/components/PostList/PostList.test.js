0c4f7ba94f66a1dc242caea37a1063846ea9b8a4
matheushenriquefs <matheus.henrique1021@gmail.com>
2022-01-02 00:18:08 -0300

    Run prettier

--- src/components/PostList/PostList.test.js
+++ src/components/PostList/PostList.test.js
@@ -6,25 +6,25 @@ import { PostList } from "./PostList";
 test("loads and display component", async () => {
   const mockedPosts = [
     {
-        title: 'Title for my first post',
-        excerpt: 'Excerpt for my first post',
-        slug: 'first-post',
-        permalink: '/posts/first-post',
-        createdAt: '12-31-2021',
+      title: "Title for my first post",
+      excerpt: "Excerpt for my first post",
+      slug: "first-post",
+      permalink: "/posts/first-post",
+      createdAt: "12-31-2021",
     },
     {
-        title: 'Title for my second post',
-        excerpt: 'Excerpt for my second post',
-        slug: 'second-post',
-        permalink: '/posts/second-post',
-        createdAt: '12-31-2021',
+      title: "Title for my second post",
+      excerpt: "Excerpt for my second post",
+      slug: "second-post",
+      permalink: "/posts/second-post",
+      createdAt: "12-31-2021",
     },
     {
-        title: 'Title for my third post',
-        excerpt: 'Excerpt for my third post',
-        slug: 'third-post',
-        permalink: '/posts/third-post',
-        createdAt: '12-31-2021',
+      title: "Title for my third post",
+      excerpt: "Excerpt for my third post",
+      slug: "third-post",
+      permalink: "/posts/third-post",
+      createdAt: "12-31-2021",
     },
   ];
 
@@ -32,6 +32,4 @@ test("loads and display component", async () => {
   const root = container.firstChild;
 
   expect(root).toBeInTheDocument();
-
-  screen.debug();
 });
